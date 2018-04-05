519005b81a1369915bb3b46b4327ede2c55dc2cd
hettmett <ruben@localhost.localdomain>
2018-04-05 15:57:39 +0400

    Added new installer with vim-plug tool

---
+++ setup_vim_plug.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# install git
+echo 'Installing git library...'
+sudo dnf install git
+echo 'Done'
+
+echo 'Installing ag search library...'
+sudo dnf install the_silver_searcher
+echo 'Done'
+
+# install vim-plug
+if [[ ! -e ~/.vim/bundle/vundle/.git ]]
+then
+    echo 'Installing vim-pug...'
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    echo 'Done'
+fi
+
+# checkout vim config files
+if [[ ! -e ~/vim_conf/.git ]]
+then
+    echo 'Installing hett vim configs...'
+    git clone https://github.com/hettmett/vim_conf.git ~/vim_conf
+    echo 'Done'
+fi
+
+# copy color file
+if [[ ! -e ~/.vim/colors ]]
+then
+    mkdir ~/.vim/colors
+    cp ~/vim_conf/*.vim ~/.vim/colors/
+fi
+
+# copy .vimrc file
+if [[ ! -e ~/.vimrc ]]
+then
+    cp ~/vim_conf/vimrc ~/.vimrc
+
+    cp ~/vim_conf/plugins.vim ~/.vim/plugins.vim
+    cp ~/vim_conf/settings.vim ~/.vim/settings.vim
+    cp ~/vim_conf/mappings.vim ~/.vim/mappings.vim
+    cp ~/vim_conf/plugins_config.vim ~/.vim/plugins_config.vim
+
+    echo 'Installing vim plugins...'
+    vim +PlugInstall +qall
+    echo 'Done'
+
+    echo 'Updating vim plugins...'
+    vim +PlugUpdate +qall
+    echo 'Done'
+fi
+
+echo 'Enjoy'
